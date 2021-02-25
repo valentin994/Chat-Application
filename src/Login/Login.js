0e24dbad80bf752b5ca529a807bf18e12b0a3e2c
Valentin Vareskic <valentin.vareskic@outlook.com>
2021-02-25 14:58:31 +0100

    added register form

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -5,6 +5,8 @@ import { Link, useHistory } from "react-router-dom";
 export default function Login(props) {
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
+    const [register, setRegister] = useState(false);
+
     let history = useHistory();
 
     const handleSubmit = (event) => {
@@ -22,17 +24,36 @@ export default function Login(props) {
             console.log(err)
         })
     }
+
+    let formFields =
+        <div>
+            <h3 className="p-2 text-white text-4xl">Sign in</h3>
+            <input type="text" onChange={(e) => setUsername(e.target.value)} value={username} className="rounded-sm px-4 py-3 mt-3 placeholder-white focus:outline-none bg-gray-500 text-white w-full" placeholder="Username" />
+            <input type="password" onChange={(e) => setPassword(e.target.value)} value={password} className="rounded-sm px-4 py-3 mt-3 placeholder-white focus:outline-none bg-gray-500 text-white w-full" placeholder="Password" />
+            <div>
+                <button className="mt-6 px-4 py-1 text-white bg-green-400 hover:bg-green-500"
+                    type="submit">Sign in</button> <Link onClick={() => setRegister(true)} className="text-white underline p-2 text-xs">You don't have an account? Register</Link>
+            </div>
+        </div>
+
+    if (register) {
+        formFields =
+            <div>
+                <h3 className="p-2 text-white text-4xl">Register</h3>
+                <input type="text" onChange={(e) => setUsername(e.target.value)} value={username} className="rounded-sm px-4 py-3 mt-3 placeholder-white focus:outline-none bg-gray-500 text-white w-full" placeholder="Name" />
+                <input type="text" onChange={(e) => setUsername(e.target.value)} value={username} className="rounded-sm px-4 py-3 mt-3 placeholder-white focus:outline-none bg-gray-500 text-white w-full" placeholder="Email" />
+                <input type="password" onChange={(e) => setPassword(e.target.value)} value={password} className="rounded-sm px-4 py-3 mt-3 placeholder-white focus:outline-none bg-gray-500 text-white w-full" placeholder="Password" />
+                <div>
+                    <button className="mt-6 px-4 py-1 text-white bg-green-400 hover:bg-green-500"
+                        type="submit">Sign in</button> <Link onClick={() => setRegister(false)} className="text-white underline p-2 text-xs">You don't have an account? Register</Link>
+                </div>
+            </div>
+    }
     return (
         <div>
             <div className="bg-gray-800 lg:w-4/12 md:6/12 w-10/12 m-auto my-10 shadow-md">
                 <form className="m-6 p-4" onSubmit={handleSubmit}>
-                    <h3 className="p-2 text-white text-4xl">Sign in</h3>
-                    <input type="text" onChange={(e) => setUsername(e.target.value)} value={username} className="rounded-sm px-4 py-3 mt-3 placeholder-white focus:outline-none bg-gray-500 text-white w-full" placeholder="Username" />
-                    <input type="password" onChange={(e) => setPassword(e.target.value)} value={password} className="rounded-sm px-4 py-3 mt-3 placeholder-white focus:outline-none bg-gray-500 text-white w-full" placeholder="Password" />
-                    <div>
-                        <button className="mt-6 px-4 py-1 text-white bg-green-400 hover:bg-green-500"
-                            type="submit">Sign in</button> <Link className="text-white underline p-2 text-xs">You don't have an account? Register</Link>
-                    </div>
+                    {formFields}
                 </form>
             </div>
         </div>
