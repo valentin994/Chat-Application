41117146e528e27a19699b65b48820588a341a07
Valentin Vareskic <valentin.vareskic@outlook.com>
2021-02-26 12:51:54 +0100

    register resolved

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -5,24 +5,40 @@ import { Link, useHistory } from "react-router-dom";
 export default function Login(props) {
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
+    const [name, setName] = useState("")
     const [register, setRegister] = useState(false);
 
     let history = useHistory();
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        axios.post("http://localhost:8000/login/", { "email": username, "password": password }, {
-            withCredentials: true, headers: {
-                'Content-Type': 'application/json;charset=UTF-8',
-                "Access-Control-Allow-Origin": "*",
-            }
-        }).then(res => {
-            props.setLoggedIn(true);
-            console.log(res);
-            history.push("/");
-        }).catch(err => {
-            console.log(err)
-        })
+        if (!register) {
+            axios.post("http://localhost:8000/login/", { "email": username, "password": password }, {
+                withCredentials: true, headers: {
+                    'Content-Type': 'application/json;charset=UTF-8',
+                    "Access-Control-Allow-Origin": "*",
+                }
+            }).then(res => {
+                props.setLoggedIn(true);
+                console.log(res);
+                history.push("/");
+            }).catch(err => {
+                console.log(err)
+            })
+        } else {
+            axios.post("http://localhost:8000/register/", { "name": name, "email": username, "password": password }, {
+                withCredentials: true, headers: {
+                    'Content-Type': 'application/json',
+                    "Access-Control-Allow-Origin": "*",
+                }
+            }).then(res => {
+                props.setLoggedIn(true);
+                console.log(res);
+                history.push("/");
+            }).catch(err => {
+                console.log(err.request.response)
+            })
+        }
     }
 
     let formFields =
@@ -40,12 +56,13 @@ export default function Login(props) {
         formFields =
             <div>
                 <h3 className="p-2 text-white text-4xl">Register</h3>
-                <input type="text" onChange={(e) => setUsername(e.target.value)} value={username} className="rounded-sm px-4 py-3 mt-3 placeholder-white focus:outline-none bg-gray-500 text-white w-full" placeholder="Name" />
+                <input type="text" onChange={(e) => setName(e.target.value)} value={name} className="rounded-sm px-4 py-3 mt-3 placeholder-white focus:outline-none bg-gray-500 text-white w-full" placeholder="Name" />
                 <input type="text" onChange={(e) => setUsername(e.target.value)} value={username} className="rounded-sm px-4 py-3 mt-3 placeholder-white focus:outline-none bg-gray-500 text-white w-full" placeholder="Email" />
+                <p className="text-red-400">Email already in use</p>
                 <input type="password" onChange={(e) => setPassword(e.target.value)} value={password} className="rounded-sm px-4 py-3 mt-3 placeholder-white focus:outline-none bg-gray-500 text-white w-full" placeholder="Password" />
                 <div>
-                    <button className="mt-6 px-4 py-1 text-white bg-green-400 hover:bg-green-500"
-                        type="submit">Sign in</button> <Link onClick={() => setRegister(false)} className="text-white underline p-2 text-xs">You don't have an account? Register</Link>
+                    <button className="mt-6 px-4 py-2 text-white bg-green-400 hover:bg-green-500"
+                        type="submit">Sign in</button> <Link onClick={() => setRegister(false)} className="text-white underline p-2 text-xs">You have an account? Sing in</Link>
                 </div>
             </div>
     }
